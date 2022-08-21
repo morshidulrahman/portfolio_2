eed563bfbb96110bd48b2a9c86fec467812762c1
morshidulrahman <boydanger416@gmail.com>
2022-08-21 17:10:52 +0600

    data

--- src/app/components/elements/PortfolioCard/index.js
+++ src/app/components/elements/PortfolioCard/index.js
@@ -5,11 +5,17 @@ function PortfolioCard({menu}) {
     <div className='flex flex-wrap items-center  justify-center md:justify-between w-full gap-10 md:gap-6'>
     {
       menu.map((item,index)=>(
-       <div key={index} className="bg-white shadow-md rounded-xl relative overflow-hidden w-[80%] md:w-[31%] category">
-          <div>
-           <img src={item.image} alt="me" /> 
+       <a 
+       target="_blank"
+       href={item.link}
+       key={index} className="bg-white shadow-md rounded-xl relative overflow-hidden w-[80%] md:w-[31%] category">
+          <div className='w-full h-[30%] overflow-hidden'>
+           <img 
+           className='object-fit w-full h-[300px]'
+           src={item.image}
+            alt="me" /> 
           </div>
-           <div className='bg-[#7B7BE8] absolute top-0 left-0 w-full h-full hoverbg opacity-0 duration-500'></div>
+           <div className='bg-[#7b7be8e1] absolute top-0 left-0 w-full h-full hoverbg opacity-0 duration-500'></div>
              <span 
               className='absolute top-0 left-4 text-white bg-[#FF4C60] px-1 py-1 rounded-b-xl category_type duration-500 -translate-y-10'
              >{item.type}</span>
@@ -19,7 +25,7 @@ function PortfolioCard({menu}) {
             <p
             className='w-8 h-8 bg-orange-500 rounded-full flex items-center justify-center text-white absolute bottom-10 left-8  hoverIcon opacity-0 duration-500'
             >{item.icon}</p>
-       </div>
+       </a>
       ))
     }
  </div>
--- src/app/data/Portfolio.js
+++ src/app/data/Portfolio.js
@@ -5,45 +5,52 @@ export const PORTFOLIO={
     items:[
         {
          
-            image:"/assets/work-1.svg",
-            title:"project mangement illustration",
+            image:"/assets/unplash.png",
+            title:"Unplash website",
             icon:<BsImage/>,
-            type:"Mobile App"
+            type:"Graphic Design",
+            link:"https://unplashapi.netlify.app/index.html"
         },
         {
          
-            image:"/assets/work-2.svg",
-            title:"project mangement illustration",
+            image:"/assets/botloader.png",
+            title:"Bootslander website",
             icon:<BsImage/>,
-            type:"Mobile App"
+            type:"Website",
+            link:"https://gracious-meitner-40452e.netlify.app/?fbclid=IwAR0wQqt1exAEtzpdBzV93P4854rBAraYibsc2bwsulVhIIr8-q7JkmTkQiw#"
+        },
+        {
+         
+            image:"/assets/musicapp.png",
+            title:"Music app Website",
+            icon:<BsImage/>,
+            type:"Mobile App",
+            link:"https://tranquil-capybara-e5f114.netlify.app/"
         },
         {
             
-            image:"/assets/work-3.svg",
-            title:"project mangement illustration",
+            image:"/assets/netlify.png",
+            title:"Netlflix Website",
             icon:<BsImage/>,
             type:"Website",
+            link:"https://lucky-zuccutto-459e25.netlify.app/"
         },
         {
          
-            image:"/assets/work-4.svg",
-            title:"project mangement illustration",
+            image:"/assets/demoProfille.png",
+            title:"Profile",
             icon:<BsImage/>,
-            type:"UI/UX Design"
+            type:"UI/UX Design",
+            link:"https://morhsidulrahman.netlify.app/"
         },
+        
         {
          
-            image:"/assets/work-5.svg",
-            title:"project mangement illustration",
+            image:"/assets/parabot.png",
+            title:"Ecommerce website",
             icon:<BsImage/>,
-            type:"Graphic Design"
-        },
-        {
-         
-            image:"/assets/work-6.svg",
-            title:"project mangement illustration",
-            icon:<BsImage/>,
-            type:"Graphic Design"
+            type:"Graphic Design",
+            link:"https://imaginative-belekoy-8d115c.netlify.app/"
         }
     ]
 }
